2f3c383c6d9667857676fc3ba3a10ae38168abd6
Oliver Lok Trevor <featherfeet5436@gmail.com>
2021-06-16 02:41:45 -0700

    Firmware now returns to home on one press of Control-C and stops/exits on two presses.

--- firmware/target/debug/firmware.d
+++
@@ -1,1 +0,0 @@
-/home/pi/satellite-antenna-rotator/firmware/target/debug/firmware: /home/pi/satellite-antenna-rotator/firmware/src/encoder.rs /home/pi/satellite-antenna-rotator/firmware/src/main.rs /home/pi/satellite-antenna-rotator/firmware/src/pid.rs /home/pi/satellite-antenna-rotator/firmware/src/thunderborg.rs
