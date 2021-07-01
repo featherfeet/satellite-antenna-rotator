be2029496d335a5ce1c3a4616a10321e1beef7a2
Oliver Trevor <featherfeet5436@gmail.com>
2021-07-01 01:35:29 -0700

    Added back the position PIDs, now feeding into the speed PIDs. Doesn't work yet.

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -43,13 +43,13 @@ fn main() {
    
     let finish = Arc::new(AtomicBool::new(false));
     let go_home = Arc::new(AtomicBool::new(false));
-    let target_altitude = Arc::new(AtomicI16::new(90));
+    // let target_altitude = Arc::new(AtomicI16::new(90));
+    let target_altitude = Arc::new(AtomicI16::new(0));
     let target_azimuth = Arc::new(AtomicI16::new(0));
 
     let gpio = Arc::new(Gpio::new().unwrap());
 
     let mut motors = Motors::new(Arc::clone(&gpio), 4, 17, 18, 23);
-    motors.set_target_speed_2(0.5);
 
     let finish_ref = Arc::clone(&finish);
     let go_home_ref = Arc::clone(&go_home);
@@ -66,20 +66,10 @@ fn main() {
         }
     }).expect("Failed to set Control-C handler!");
 
-    while !finish.load(Ordering::Relaxed) {
-        std::thread::sleep(std::time::Duration::from_millis(50));
-    }
-    motors.finish();
-
-    /*
-    let gpio_ref = Arc::clone(&gpio);
     let target_altitude_ref = Arc::clone(&target_altitude);
     let target_azimuth_ref = Arc::clone(&target_azimuth);
     thread::spawn(move || {
-        let azimuth_encoder = Encoder::new(&gpio_ref, 18, 23);
-        let altitude_encoder = Encoder::new(&gpio_ref, 4, 17);
-
-        *(altitude_encoder.steps.lock().unwrap()) = (altitude_angle_to_driving_revs(90.0) * ALTITUDE_ENCODER_STEPS_PER_REVOLUTION) as i64;
+        //*(altitude_encoder.steps.lock().unwrap()) = (altitude_angle_to_driving_revs(90.0) * ALTITUDE_ENCODER_STEPS_PER_REVOLUTION) as i64;
 
         let mut azimuth_pid = Pid::new(-2.0, -0.005, -20.0, -1.0, 1.0);
         let mut altitude_pid = Pid::new(-2.0, -0.005, -20.0, -1.0, 1.0);
@@ -87,26 +77,23 @@ fn main() {
         //altitude_pid.set_logfile("altitude_encoder.csv");
 
         while !finish.load(Ordering::Relaxed) {
-            let azimuth_steps: i64 = *azimuth_encoder.steps.lock().unwrap();
-            let altitude_steps: i64 = *altitude_encoder.steps.lock().unwrap();
-
-            let azimuth_revs: f64 = azimuth_steps as f64 / AZIMUTH_ENCODER_STEPS_PER_REVOLUTION;
-            let altitude_revs: f64 = altitude_steps as f64 / ALTITUDE_ENCODER_STEPS_PER_REVOLUTION;
+            let altitude_revs: f64 = motors.get_revs_1();
+            let azimuth_revs: f64 = motors.get_revs_2();
 
             let target_revs_driving_altitude = altitude_angle_to_driving_revs(target_altitude_ref.load(Ordering::Relaxed) as f64);
-            let altitude_motor_power: f64 = altitude_pid.compute(altitude_revs, target_revs_driving_altitude);
+            println!("target_revs_driving_altitude: {}", target_revs_driving_altitude);
+            let altitude_motor_target_speed: f64 = altitude_pid.compute(altitude_revs, target_revs_driving_altitude);
 
             let target_revs_driving_azimuth = azimuth_angle_to_driving_revs(target_azimuth_ref.load(Ordering::Relaxed) as f64);
-            let azimuth_motor_power: f64 = azimuth_pid.compute(azimuth_revs, target_revs_driving_azimuth);
+            let azimuth_motor_target_speed: f64 = azimuth_pid.compute(azimuth_revs, target_revs_driving_azimuth);
 
-            thunderborg.set_motor_1(altitude_motor_power);
-            thunderborg.set_motor_2(azimuth_motor_power);
+            motors.set_target_speed_1(altitude_motor_target_speed);
+            motors.set_target_speed_2(azimuth_motor_target_speed);
 
             std::thread::sleep(std::time::Duration::from_millis(10));
         }
 
-        thunderborg.set_motor_1(0.0);
-        thunderborg.set_motor_2(0.0);
+        motors.finish();
         std::process::exit(0);
     });
 
@@ -114,24 +101,22 @@ fn main() {
     let tle: Tle = Tle::from_file("ISS (ZARYA)", "iss.tle").unwrap();
     //let tle: Tle = Tle::from_file("LUSAT (LO-19)", "amateur.tle").unwrap();
     // HOME:
-    //let location: Location = Location { lat_deg: 37.649250, lon_deg: -121.875070, alt_m: 105.0 };
+    let location: Location = Location { lat_deg: 37.649250, lon_deg: -121.875070, alt_m: 105.0 };
     // HILL:
-    let location: Location = Location { lat_deg: 37.650444, lon_deg: -121.866836, alt_m: 171.0 };
+    //let location: Location = Location { lat_deg: 37.650444, lon_deg: -121.866836, alt_m: 171.0 };
     let mut predict: Predict = Predict::new(&tle, &location);
 
     //predict.update(None);
     //println!("{:#?}", predict);
 
     loop {
-	/*
         println!("Target altitude?");
         let target_altitude_input: i16 = read!();
         target_altitude.store(target_altitude_input, Ordering::Relaxed);
         println!("Target azimuth?");
         let target_azimuth_input: i16 = read!();
         target_azimuth.store(target_azimuth_input, Ordering::Relaxed);
-	*/
-
+        /*
         if go_home.load(Ordering::Relaxed) {
             target_altitude.store(90, Ordering::Relaxed);
 	    target_azimuth.store(0, Ordering::Relaxed);
@@ -147,6 +132,6 @@ fn main() {
             target_azimuth.store(predict.sat.az_deg as i16, Ordering::Relaxed);
         }
         std::thread::sleep(std::time::Duration::from_millis(500));
+        */
     }
-    */
 }
--- firmware/src/motors.rs
+++ firmware/src/motors.rs
@@ -11,16 +11,32 @@ use std::sync::atomic::{ AtomicBool, Ordering };
 const STEPS_CHANGED_BUFFER_SIZE: usize = 5;
 const STEPS_PER_REVOLUTION: f64 = 897.96;
 
+/// Represents a single speed-controlled motor.
 struct Motor {
+    /// Quadrature encoder for this motor.
     encoder: Encoder,
+    /// PID used to control the speed of this motor.
     pid: Pid,
+    /// Number of encoder steps from the previous time that update() was called. Used to calculate speed.
     prev_steps: i64,
+    /// Time since Unix epoch (in microseconds) since the last time that update() was called. Used to calculate speed.
     prev_time: u128,
+    /// Buffer of steps_changed (number of steps between calls to update()) values. Used to smooth out noise introduced by quantization error.
     steps_changed_buffer: [i64; STEPS_CHANGED_BUFFER_SIZE],
+    /// Index to write to in steps_changed_buffer. Circles around back to 0 when it reaches STEPS_CHANGED_BUFFER_SIZE so that the buffer is "circular."
     steps_changed_buffer_index: usize
 }
 
 impl Motor {
+    /// Create a motor.
+    ///
+    /// # Arguments
+    ///
+    /// * `gpio` - Arc pointing to a RPPAL Gpio object.
+    ///
+    /// * `encoder_channel_a_pin` - GPIO pin number for channel A of the quadrature encoder.
+    ///
+    /// * `encoder_channel_b_pin` - GPIO pin number for channel B of the quadrature encoder.
     pub fn new(gpio: Arc::<Gpio>, encoder_channel_a_pin: u8, encoder_channel_b_pin: u8) -> Motor {
         let encoder = Encoder::new(gpio, encoder_channel_a_pin, encoder_channel_b_pin);
         let mut pid = Pid::new(-2.0, -0.025, -1.8, -1.0, 1.0);
@@ -28,7 +44,16 @@ impl Motor {
         Motor { encoder: encoder, pid: pid, prev_steps: 0, prev_time: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros(), steps_changed_buffer: [0_i64; STEPS_CHANGED_BUFFER_SIZE], steps_changed_buffer_index: 0 }
     }
 
-    pub fn update_and_compute_power_level(&mut self, target_speed: f64) -> f64 {
+    /// Update the state of the motor.
+    ///
+    /// # Arguments
+    ///
+    /// * `target_speed` - The target speed for the motor PID, given in revolutions per second.
+    ///
+    /// Returns a tuple of two floats. The first float is the power level (from -1.0 to 1.0) that
+    /// should be sent to the motor. The second float is the current motor speed in revolutions per
+    /// second.
+    pub fn update(&mut self, target_speed: f64) -> (f64, f64) {
         let time: u128 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros();
         let time_elapsed: u128 = time - self.prev_time;
 
@@ -51,37 +76,69 @@ impl Motor {
         self.prev_time = time;
         self.prev_steps = steps;
 
-        self.pid.compute(speed, target_speed)
+        let power: f64 = self.pid.compute(speed, target_speed);
+        
+        let revs: f64 = steps as f64 / STEPS_PER_REVOLUTION;
+
+        (power, revs)
     }
 }
 
+/// Represents two speed-controlled motors. Does NOT handle position control, only speed. This struct's job is to keep the two motors running as close to their target speeds as possible.
 pub struct Motors {
+    /// Set this to true to stop the motors.
     finish: Arc::<AtomicBool>,
+    /// Target speed (in revolutions per second) for motor 1.
     target_speed_1: Arc::<AtomicF64>,
+    /// Target speed (in revolutions per second) for motor 2.
     target_speed_2: Arc::<AtomicF64>,
+    /// Total number of revolutions done by motor 1 (decreases if the motor turns backwards, increases if it turns forwards).
+    revs_1: Arc::<AtomicF64>,
+    /// Total number of revolutions done by motor 2 (decreases if the motor turns backwards, increases if it turns forwards).
+    revs_2: Arc::<AtomicF64>,
+    /// Handle for the thread that runs the motor speed PIDs.
     control_thread: thread::JoinHandle::<()>
 }
 
 impl Motors {
+    /// Create a Motors structure.
+    ///
+    /// # Arguments
+    ///
+    /// * `gpio` - Arc pointing to a RPPAL Gpio object.
+    ///
+    /// * `encoder_1_channel_a_pin` - GPIO pin number for channel A of the encoder for motor 1.
+    ///
+    /// * `encoder_1_channel_b_pin` - GPIO pin number for channel B of the encoder for motor 1.
+    ///
+    /// * `encoder_2_channel_a_pin` - GPIO pin number for channel A of the encoder for motor 2.
+    ///
+    /// * `encoder_2_channel_b_pin` - GPIO pin number for channel B of the encoder for motor 2.
     pub fn new(gpio: Arc::<Gpio>, encoder_1_channel_a_pin: u8, encoder_1_channel_b_pin: u8, encoder_2_channel_a_pin: u8, encoder_2_channel_b_pin: u8) -> Motors {
         let finish = Arc::new(AtomicBool::new(false));
         let target_speed_1 = Arc::new(AtomicF64::new(0.0));
         let target_speed_2 = Arc::new(AtomicF64::new(0.0));
+        let revs_1 = Arc::new(AtomicF64::new(0.0));
+        let revs_2 = Arc::new(AtomicF64::new(0.0));
 
         let finish_ref = Arc::clone(&finish);
         let target_speed_1_ref = Arc::clone(&target_speed_1);
         let target_speed_2_ref = Arc::clone(&target_speed_2);
+        let revs_1_ref = Arc::clone(&revs_1);
+        let revs_2_ref = Arc::clone(&revs_2);
         let control_thread = thread::spawn(move || {
             let mut thunderborg = Thunderborg::new(0x19);
             let mut motor_1 = Motor::new(Arc::clone(&gpio), encoder_1_channel_a_pin, encoder_1_channel_b_pin);
             let mut motor_2 = Motor::new(Arc::clone(&gpio), encoder_2_channel_a_pin, encoder_2_channel_b_pin);
 
             while !finish_ref.load(Ordering::Relaxed) {
-                let power_1: f64 = motor_1.update_and_compute_power_level(target_speed_1_ref.load(Ordering::Relaxed));
-                let power_2: f64 = motor_2.update_and_compute_power_level(target_speed_2_ref.load(Ordering::Relaxed));
+                let (power_1, revs_1) = motor_1.update(target_speed_1_ref.load(Ordering::Relaxed));
+                let (power_2, revs_2) = motor_2.update(target_speed_2_ref.load(Ordering::Relaxed));
 
                 thunderborg.set_motor_1(power_1);
                 thunderborg.set_motor_2(power_2);
+                revs_1_ref.store(revs_1, Ordering::Relaxed);
+                revs_2_ref.store(revs_2, Ordering::Relaxed);
 
                 std::thread::sleep(std::time::Duration::from_millis(5));
             }
@@ -90,17 +147,30 @@ impl Motors {
             thunderborg.set_motor_2(0.0);
         });
 
-        Motors { finish: finish, target_speed_1: target_speed_1, target_speed_2: target_speed_2, control_thread: control_thread }
+        Motors { finish: finish, target_speed_1: target_speed_1, target_speed_2: target_speed_2, revs_1: revs_1, revs_2: revs_2, control_thread: control_thread }
     }
 
+    /// Set the target speed (in revolutions per second) for the speed PID of motor 1.
     pub fn set_target_speed_1(&mut self, target_speed_1: f64) {
         self.target_speed_1.store(target_speed_1, Ordering::Relaxed);
     }
 
+    /// Set the target speed (in revolutions per second) for the speed PID of motor 2.
     pub fn set_target_speed_2(&mut self, target_speed_2: f64) {
         self.target_speed_2.store(target_speed_2, Ordering::Relaxed);
     }
 
+    /// Get the total number of revolutions done by motor 1 (increases for forward rotation, decreases for backward).
+    pub fn get_revs_1(&mut self) -> f64 {
+        return self.revs_1.load(Ordering::Relaxed);
+    }
+
+    /// Get the total number of revolutions done by motor 2 (increases for forward rotation, decreases for backward).
+    pub fn get_revs_2(&mut self) -> f64 {
+        return self.revs_2.load(Ordering::Relaxed);
+    }
+
+    /// Stop the motors and end the motor control thread.
     pub fn finish(self) {
         self.finish.store(true, Ordering::Relaxed);
         self.control_thread.join().expect("Failed to join motor control thread!");
